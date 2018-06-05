7db25fd3410e0a289afcbf905a14059fffffc5b4
junqiannHao <1635920137@qq.com>
2018-06-05 16:25:51 +0800

    冒泡算法完成

--- src/basic_class_01/Code_00_BubbleSort.java
+++ src/basic_class_01/Code_00_BubbleSort.java
@@ -1,7 +1,33 @@
 package basic_class_01;
+
 //冒泡算法
 public class Code_00_BubbleSort {
-	
-	
+	public static void bubble(int[] arr) {
+		// 数组为空或者长度为1直接返回
+		if (arr == null || arr.length < 2) {
+			return;
+		}
 
+		for (int end = arr.length - 1; end > 0; end--) {
+			for (int i = 0; i < end; i++) {
+				if (arr[i] > arr[i + 1]) {
+					swap(arr, i, i + 1);
+				}
+			}
+		}
+	}
+
+	public static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 4545, 3432, 667, 231, 4543, 21321, 76, 23 };
+		bubble(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + ",");
+		}
+	}
 }
